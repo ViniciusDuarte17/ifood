76bad23bdbace26900c2b682ee3780141b8974bb
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-12-05 13:05:29 -0300

    teste component footer

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -15,3 +15,4 @@ export const Funter = () => {
 // Parei ná página de fazer o footer do feed da home page
 // tava querendo colocar para poder fazer a parte do cart
 // e depois pegar as informações do perfil
+// muito boa anotação
