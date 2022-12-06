99b6d9e0731dd764b04877591352a6df62a3981b
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-12-06 15:24:53 -0300

    feat: adicionando campo de pesquisa no feed

--- src/pages/Home/Feed/index.jsx
+++ src/pages/Home/Feed/index.jsx
@@ -1,13 +1,35 @@
+import * as Styled from "./styled";
 import React from "react";
 import { Headers } from "../../../components/Hearder";
-import {useProtectedPage} from "../../../hooks/useProtectedPage";
+import { useProtectedPage } from "../../../hooks/useProtectedPage";
+import SearchIcon from '@mui/icons-material/Search';
+import TextField from '@mui/material/TextField';
+import { InputAdornment } from "@mui/material";
+
 
 export const FeedPage = () => {
   useProtectedPage();
 
   return (
-    <div>
-      <Headers texto={"Ifuture"}/>
-    </div>
+    <>
+      <Headers texto={"Ifuture"} />
+      <TextField
+        placeholder="Restaurante"
+        fullWidth
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <SearchIcon />
+            </InputAdornment>
+          ),
+        }}
+        variant="outlined"
+      />
+      <Styled.Filter>
+        <span>
+          Burger
+        </span>
+      </Styled.Filter>
+    </>
   )
 }
---
+++ src/pages/Home/Feed/styled.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+
+export const Filter = styled.div`
+     width: 95%;
+     height: 2.625rem;
+     margin: 0.5rem 0 0;
+     padding: 0.75rem 0 0.75rem 1rem;
+     border: solid green 1px;
+`
+
