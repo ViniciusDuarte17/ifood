5130cf7ee5e7e3aa26388b005909058fe81f0ccb
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-12-06 15:46:35 -0300

    FEAT: efeito sleid

--- src/pages/Home/Feed/index.jsx
+++ src/pages/Home/Feed/index.jsx
@@ -5,10 +5,17 @@ import { useProtectedPage } from "../../../hooks/useProtectedPage";
 import SearchIcon from '@mui/icons-material/Search';
 import TextField from '@mui/material/TextField';
 import { InputAdornment } from "@mui/material";
+import { Slied } from "../../../components/Slied";
+import { SwiperSlide } from "swiper/react";
 
 
 export const FeedPage = () => {
   useProtectedPage();
+
+  const settings = {
+    spaceBetween: 5,
+    slidesPerView: 4
+  }
 
   return (
     <>
@@ -26,9 +33,11 @@ export const FeedPage = () => {
         variant="outlined"
       />
       <Styled.Filter>
-        <span>
-          Burger
-        </span>
+        <Slied settings={settings}>
+          <SwiperSlide>
+            <Styled.Text>lanche</Styled.Text>
+          </SwiperSlide>
+        </Slied>
       </Styled.Filter>
     </>
   )
--- src/pages/Home/Feed/styled.js
+++ src/pages/Home/Feed/styled.js
@@ -6,6 +6,22 @@ export const Filter = styled.div`
      height: 2.625rem;
      margin: 0.5rem 0 0;
      padding: 0.75rem 0 0.75rem 1rem;
-     border: solid green 1px;
+     /* border: solid green 1px; */
+`
+
+
+export const Text = styled.span`
+   width: 5.438rem;
+  height: 1.125rem;
+  margin: 0 0.5rem 0 0;
+  font-family: Roboto;
+  font-size: 1rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  letter-spacing: -0.39px;
+  text-align: center;
+  /* color: var(--lipstick); */
 `
 
