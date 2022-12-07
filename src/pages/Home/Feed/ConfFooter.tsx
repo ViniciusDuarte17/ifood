e0024ab893a5edc5a29ca791e142a9c1b269164b
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-12-07 15:01:02 -0300

    refactore: Component Feed

---
+++ src/pages/Home/Feed/ConfFooter.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+import * as Styled from "./styled";
+import { Footer } from "../../../components/footer";
+import HomeIcon from '@mui/icons-material/Home';
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+import PersonIcon from '@mui/icons-material/Person';
+import { Button } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { goToCart, goToProfile } from "../../../router/coordinator";
+
+export const CongFooter = () => {
+    const navigate = useNavigate();
+    return(
+        <>
+          <Footer>
+        <Styled.ContentIcons>
+          <div>
+            <Button>
+              <HomeIcon color="primary" fontSize="large" />
+            </Button>
+          </div>
+          <div>
+            <Button onClick={() => goToCart(navigate)}>
+              <ShoppingCartIcon color="primary" fontSize="large" />
+            </Button>
+          </div>
+          <div>
+            <Button onClick={() => goToProfile(navigate)}>
+              <PersonIcon color="primary" fontSize="large" />
+            </Button>
+          </div>
+        </Styled.ContentIcons>
+      </Footer>
+        </>
+    )
+}
--- src/pages/Home/Feed/index.jsx
+++ src/pages/Home/Feed/index.jsx
@@ -4,21 +4,15 @@ import { Headers } from "../../../components/Hearder";
 import { useProtectedPage } from "../../../hooks/useProtectedPage";
 import SearchIcon from '@mui/icons-material/Search';
 import TextField from '@mui/material/TextField';
-import { Button, InputAdornment } from "@mui/material";
+import { InputAdornment } from "@mui/material";
 import { Slied } from "../../../components/Slied";
 import { SwiperSlide } from "swiper/react";
 import { Card } from "../../../components/Card";
-import { Footer } from "../../../components/footer";
-import HomeIcon from '@mui/icons-material/Home';
-import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
-import PersonIcon from '@mui/icons-material/Person';
-import { useNavigate } from "react-router-dom";
-import { goToCart, goToProfile } from "../../../router/coordinator";
+import { CongFooter } from "./ConfFooter";
 
 
 export const FeedPage = () => {
   useProtectedPage();
-  const navigate = useNavigate();
 
   const settings = {
     spaceBetween: 5,
@@ -51,30 +45,8 @@ export const FeedPage = () => {
       </Styled.Filter>
       <Styled.ContentCard>
         <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
       </Styled.ContentCard>
-      <Footer>
-        <Styled.ContentIcons>
-          <div>
-            <Button>
-              <HomeIcon color="primary" fontSize="large" />
-            </Button>
-          </div>
-          <div>
-            <Button onClick={()=> goToCart(navigate)}>
-              <ShoppingCartIcon color="primary" fontSize="large" />
-            </Button>
-          </div>
-          <div>
-            <Button onClick={() => goToProfile(navigate)}>
-              <PersonIcon color="primary" fontSize="large" />
-            </Button>
-          </div>
-        </Styled.ContentIcons>
-      </Footer>
+      <CongFooter />
     </Styled.ContainerFeed>
   )
 }
