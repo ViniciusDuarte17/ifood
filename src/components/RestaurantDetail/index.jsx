7e3231ef52673b19768be5593dc2c9fccde1d93f
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-12-16 14:31:46 -0300

    estrutura restauratnDetail

--- src/components/RestaurantDetail/index.jsx
+++ src/components/RestaurantDetail/index.jsx
@@ -11,14 +11,16 @@ export const RestaurantDetail = (props) => {
         <Styled.ContentImg>
         <CardMedia
           component="img"
-          height="194"
+          height="111"
           image={"https://firebasestorage.googleapis.com/v0/b/missao-newton.appspot.com/o/futureFoodsRestaurants%2Fhabibs.jpg?alt=media&token=a30ea547-3a3b-4e80-b58e-b8dc976697de"}
           alt="Logo restaurante"
         />
         </Styled.ContentImg>
-        <div>
-            <p>Bullguer</p>
-        </div>
+        <Styled.RestaurentItem>
+            <Styled.SpanName>Bullguer</Styled.SpanName>
+            <Styled.SpanDescription>Pão,carne.queijo,piclesemolho.</Styled.SpanDescription>
+            <Styled.SpanPrice>R$23,00</Styled.SpanPrice>
+        </Styled.RestaurentItem>
       </Styled.Content>
     </Styled.Container>
   );
--- src/components/RestaurantDetail/styled.js
+++ src/components/RestaurantDetail/styled.js
@@ -4,9 +4,12 @@ import styled from "styled-components";
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    width: 100%;
+    width: 90%;
+    margin: auto;
+    height: 7rem;
     border-radius: 8px;
     border: solid 1px #b8b8b8;
+    margin-top: 2%;
 `
 
 export const Content = styled.main`
@@ -14,6 +17,47 @@ export const Content = styled.main`
 `
 
 export const ContentImg = styled.div`
-    width: 34%;
-    /* margin: 2% 5%; */
+    width: 30%;
+`
+
+export const RestaurentItem = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 90%;
+`
+
+export const SpanName = styled.span`
+  font-family: Roboto;
+  font-size: 1rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  padding: 0 8%;
+  margin-top: 6%;
+  color:#e8222e;
+`
+
+export const SpanDescription = styled.span`
+  font-family: Roboto;
+  font-size: 1rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  padding: 0 8%;
+  color:#b8b8b8;
+  margin-top: 3%;
+`
+
+export const SpanPrice = styled.span`
+  font-family: Roboto;
+  font-size: 1rem;
+  font-weight: normal;
+  font-stretch: normal;
+  font-style: normal;
+  line-height: normal;
+  padding: 0 8%;
+  color:black;
+  margin-top: 3%;
 `
