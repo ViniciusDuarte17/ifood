44c3a6ea8001eb58e7514b397dfcfcd53e3bc0f2
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2022-12-19 15:17:14 -0300

    Restaurant formatando o texto de código

--- src/pages/Home/Restaurants/index.jsx
+++ src/pages/Home/Restaurants/index.jsx
@@ -15,10 +15,10 @@ export const RestaurantPage = () => {
   const params = useParams();
   const [restaurantDetail, setRestaurantDetail] = useState([])
 
-  useEffect( () => {
+  useEffect(() => {
     getRestaurantDetail(params.id, setRestaurantDetail)
   }, [])
-  
+
   return (
     <div>
       <Headers texto={"Restaurante"}>
@@ -33,7 +33,7 @@ export const RestaurantPage = () => {
         {
           restaurantDetail &&
           restaurantDetail.map((restaurant) => {
-            return <RestaurantDetail key={restaurant.id} restaurant={restaurant}/>
+            return <RestaurantDetail key={restaurant.id} restaurant={restaurant} />
           })
         }
       </Styled.ContentMain>
